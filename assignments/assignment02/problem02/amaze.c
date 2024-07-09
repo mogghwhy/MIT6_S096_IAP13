c4f0cdb536c614d0bd89ba1e54820f15fd6bed3e
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-07-09 12:50:14 +0300

    added amazeWOW

--- assignments/assignment02/problem02/amaze.c
+++ assignments/assignment02/problem02/amaze.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void amazeWOW(void) {
+
+    int i = 0;
+    printf("amazeWOW:\t");
+    for(i = 0; i <= 10; i++) {
+        if (i % 2 == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
 int main(int argc, char ** argv){
+    amazeWOW();
 return 0;
 }
