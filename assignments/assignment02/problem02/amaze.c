22a03ca1acb050213c97e1c846356c3a4323be86
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-07-09 13:06:21 +0300

    added amaze4

--- assignments/assignment02/problem02/amaze.c
+++ assignments/assignment02/problem02/amaze.c
@@ -38,9 +38,49 @@ void amaze3(void) {
     printf("\n");
 }
 
+void amaze4(void) {
+    int i = 0;
+    printf("amaze4:\t\t");
+    i++;
+    if (i == 1) {
+        printf("%d ", i);
+        i++;
+    }
+    if (i == 2) {
+        i++;
+    }
+    if (i == 3) {
+        printf("%d ", i);
+        i++;
+    }
+    if (i == 4) {
+        i++;
+    }    
+    if (i == 5) {
+        printf("%d ", i);
+        i++;
+    }
+    if (i == 6) {
+        i++;
+    }    
+    if (i == 7) {
+        printf("%d ", i);
+        i++;
+    }
+    if (i == 8) {
+        i++;
+    }    
+    if (i == 9) {
+        printf("%d ", i);
+        i++;
+    }
+    printf("\n");
+}
+
 int main(int argc, char ** argv){
     amazeWOW();
     amaze2();
     amaze3();
+    amaze4();    
 return 0;
 }
