72b61ea34ba8a24d82e7d254fd4856ce36a9e055
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-07-10 11:24:08 +0300

    solution to exercise 2 for lab3

--- labs/lab03/exercise02/main.c
+++ labs/lab03/exercise02/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 
 int main(void)
 {
-    char str[];
-    ???(,"hakuna matata!");
+    char *str = (char *) malloc( 15 * sizeof(char));
+    strcpy(str,"hakuna matata!");
     printf("%s\n", str);
     return 0;
 }
