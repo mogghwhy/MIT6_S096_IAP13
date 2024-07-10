46a92f0830c459c5e9b3d90012a1368a9b8ab691
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-07-10 15:52:39 +0300

    assignment 3 exercise 2

--- assignments/assignment03/resize.c
+++ assignments/assignment03/resize.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 
 int main(){
@@ -12,14 +13,14 @@ int main(){
 	scanf("%d",&n1);
 
 	//Create a new array of n1 ints
-	int* a1 = /* Fill in*/
+	int* a1 = malloc(n1 * sizeof(int));
 	int i;
 	for(i=0; i<n1; i++){
 		//Set each value in a1 to 100
-		/*Fill in*/=100
+		*(a1 + i) = 100;
 		
 		//Print each element out (to make sure things look right)
-		printf("%d ",/*Fill in*/ );
+		printf("%d ",*(a1+i) );
 	}
 
 	//User specifies the new array size, stored in variable n2.
@@ -28,23 +29,28 @@ int main(){
 	scanf("%d",&n2);
 
 	//Dynamically change the array to size n2
-	a1 = /* Fill in*/
+	a1 = realloc(a1, n2 * sizeof(int));
 
 	//If the new array is a larger size, set all new members to 0. Reason: dont want to use uninitialized variables.
 
-	/*
-	Fill
-	In
-	*/
+	if(n2 > n1) {
+		
+		//int h = n2 - n1;
+		for(int s = n1; s < n2; s++) {
+			printf("it goes in\n");
+			*(a1 + s) = 0;
+		}
+	}
 	
 
 	for(i=0; i<n2;i++){
 		//Print each element out (to make sure things look right)
-		printf("%d ",/* Fill in */);
+		printf("%d ", *(a1+i));/* Fill in */
 	}
 	printf("\n");
 	
 	//Done with array now, done with program :D
+	free(a1);
 	
 	return 0;
 }
