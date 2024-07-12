a094f071858415636cb01dbdb9960c065d3ccd30
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-07-12 11:20:19 +0300

    defined node structure members

--- assignments/assignment04/bintree.h
+++ assignments/assignment04/bintree.h
@@ -2,8 +2,12 @@
 #define BINTREE_H
 
 // Node structure should have an int node_id, an int data, and pointers to left and right child nodes
-typedef struct node {
-
+typedef struct node
+{
+    int node_id;
+    int data;
+    struct node *left;
+    struct node *right;
 } node;
 
 ///*** DO NOT CHANGE ANY FUNCTION DEFINITIONS ***///
