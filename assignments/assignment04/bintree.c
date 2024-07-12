689b928c63fd59b2ad5f6e2237b28c19928cc29c
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-07-12 16:54:55 +0300

    some more progress saved

--- assignments/assignment04/bintree.c
+++ assignments/assignment04/bintree.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "bintree.h"
 
 ///*** DO NOT CHANGE ANY FUNCTION DEFINITIONS ***///
@@ -8,30 +9,35 @@
 node *root = NULL;
 
 // Insert a new node into the binary tree with node_id and data
-void insert_node(int node_id, int data) {
-    node* new_node = (node*) malloc(sizeof(node));
+void insert_node(int node_id, int data)
+{
+    node *new_node = (node *)malloc(sizeof(node));
     new_node->data = data;
     new_node->node_id = node_id;
     new_node->left = NULL;
     new_node->right = NULL;
-    bool not_set = true;    
-    while(not_set) {
-        node* parent = root;
-    }
-    if (root == NULL) {
-        root = new_node;
+    bool not_set = true;
+    node *parent = root;
+    while (not_set)
+    {
+        if (parent == NULL)
+        {
+            parent = new_node;
+            not_set = false;
+        }
     }
 }
 
 // Find the node with node_id, and return its data
-int find_node_data(int node_id) {
-    return 0;	
+int find_node_data(int node_id)
+{
+    return 0;
 }
 
 ///***** OPTIONAL: Challenge yourself w/ deletion if you want ***///
-/*//Find and remove a node in the binary tree with node_id. 
+/*//Find and remove a node in the binary tree with node_id.
 //Children nodes are fixed appropriately.
 void remove_node(int node_id) {
-	
+
 }
 */
